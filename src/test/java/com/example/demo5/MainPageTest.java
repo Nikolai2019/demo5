0a9bcf6d6ab37b1acb5b0c512f1be3de69e9d361
Nikolai2019 <kolya.ruslyakov@gmail.com>
2023-08-10 18:03:28 +0500

    checkUrlTest created

--- src/test/java/com/example/demo5/MainPageTest.java
+++ src/test/java/com/example/demo5/MainPageTest.java
@@ -1,12 +1,20 @@
 package com.example.demo5;
+
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -36,9 +44,32 @@ public class MainPageTest {
         searchField.submit();
 
         WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"),"Значение в поле ввода изменилось");
+        assertEquals(input, searchPageField.getAttribute("value"), "Значение в поле ввода изменилось");
     }
 
+    @Test
+    public void checkUrlTest() {
+        String input = "Selenium";
+        WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
+        searchField.sendKeys(input);
+        searchField.submit();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        ));
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        clickElement(results, 0);
+
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        assertEquals("https://www.selenium.dev/", driver.getCurrentUrl(), "URL отличаются");
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
+    }
 }
 
 
